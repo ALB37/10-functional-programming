ad5f34250f1ab446f9bf778bd245016a337f169a
Andrew Bloom <blooma116@gmail.com>
2017-11-06 09:03:58 -0800

    finished lab

--- andrew/public/scripts/articleView.js
+++ andrew/public/scripts/articleView.js
@@ -138,7 +138,7 @@ var app = app || {};
   articleView.initAdminPage = () => {
   // DONE: Call the Handlebars .compile() method, which will return a function for you to use where needed.
   // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since we are then calling "template" below.
-    const template = Handlebars.compile();
+    let template = Handlebars.compile($('#admin-template').text());
 
     // REVIEWED: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM.
     // The callback is not required to return anything.
